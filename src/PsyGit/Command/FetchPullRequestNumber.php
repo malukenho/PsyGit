9a10e3d6072c1cb0dd69f9cd7958992dab0e8f09
Jefersson Nathan <admin@phpse.net>
2016-01-10 06:38:32 -0300

    Fetch a pull request number

---
+++ src/PsyGit/Command/FetchPullRequestNumber.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+declare(strict_types=1);
+
+namespace PsyGit\Command;
+
+/**
+ * Fetch a pull request to a new branch {@link https://git-scm.com/docs/git-fetch}
+ *
+ * @author Jefersson Nathan <malukenho@phpse.net>
+ */
+final class FetchPullRequestNumber
+{
+    const DEFAULT_REMOTE = 'origin';
+
+    /**
+     * @var callable
+     */
+    private $commandHandler;
+
+    /**
+     * @param callable $commandHandler
+     */
+    public function __construct(callable $commandHandler)
+    {
+        $this->commandHandler = $commandHandler;
+    }
+
+    public function __invoke($directory, $pullRequest, $branch, $remote = self::DEFAULT_REMOTE)
+    {
+        $commandHandler = $this->commandHandler;
+
+        $pullRequest = escapeshellarg($pullRequest);
+        $directory   = escapeshellarg($directory);
+        $branch      = escapeshellarg($branch);
+        $remote      = escapeshellarg($remote);
+
+        return $commandHandler("cd $directory && git fetch $remote pull/$pullRequest/head:$branch");
+    }
+}
